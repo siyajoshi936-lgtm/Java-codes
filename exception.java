f07600ac01e52cff9fbf9443204bc47413e2cf27
siyajoshi936-lgtm <siyajoshi936@gmail.com>
2026-03-05 20:06:03 +0530

    Add files via upload

---
+++ exception.java
@@ -0,0 +1,9 @@
+public class exception {
+    public static void main(String[] args) {
+        int a=10;
+        int b=0;
+        int result=a/b;
+        System.out.println(result);
+        System.out.println("Program finished");
+    }
+}
